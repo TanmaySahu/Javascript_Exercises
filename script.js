244cdca555cb9eb0fea7ec9435940a3c3d82445d
TanmaySahu <tanmaysahu839926@gmail.com>
2021-03-23 18:57:33 +0530

    string manupulation : vowel remover

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+function vowelRemover(){
+    var str = document.getElementById("ipstr").value;
+    document.getElementById("vrRes").innerHTML = str.replace(/[aeiouAEIOU]/g, '');
+}
+
 function Addition(){
-    var fnum = document.getElementById("fnum").value
-    var snum = document.getElementById("snum").value
-    alert(Number(fnum)+Number(snum))
+    var fnum = document.getElementById("fnum").value;
+    var snum = document.getElementById("snum").value;
+    alert(Number(fnum)+Number(snum));
 }
 
